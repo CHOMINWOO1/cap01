b00bd4c40497e15cfabd95685614f92fca03ccc9
Dubidubab <44222753+Dubidubab@users.noreply.github.com>
2019-12-07 16:49:03 +0900

    jiji

--- app/src/main/java/com/example/cap01/FoodActivity.java
+++ app/src/main/java/com/example/cap01/FoodActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FoodListActivity extends AppCompatActivity {
+public class FoodActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_food_list);
+        setContentView(R.layout.activity_food);
     }
 }
