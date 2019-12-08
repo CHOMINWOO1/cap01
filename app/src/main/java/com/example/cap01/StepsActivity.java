32a126dcfba7e0b08a96184608ed8fa7d69dcfc0
minwoo <kcjmw1208@naver.com>
2019-12-08 16:02:02 +0900

    commit

--- app/src/main/java/com/example/cap01/StepsActivity.java
+++ app/src/main/java/com/example/cap01/StepsActivity.java
@@ -2,19 +2,37 @@ package com.example.cap01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class StepsActivity extends AppCompatActivity {
+public class StepsActivity extends AppCompatActivity implements SensorEventListener {
 
+    private SensorManager sensorManager;
+    private Sensor stepDetectorSensor;
+    TextView tvStepDetector;
+    private int mStepDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steps);
+
+        tvStepDetector = (TextView)findViewById(R.id.tvStepDetector);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        stepDetectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        if(stepDetectorSensor == null) {
+            Toast.makeText(this, "No Step Detect Sensor", Toast.LENGTH_SHORT).show();
+        }
 
         Button buttonHome=(Button)findViewById(R.id.buttonHome);
         Button buttonSteps=(Button)findViewById(R.id.buttonSteps);
@@ -42,5 +60,31 @@ public class StepsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, stepDetectorSensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            if(event.values[0] == 1.0f) {
+                mStepDetector++;
+                tvStepDetector.setText("Step Detect : " + String.valueOf(mStepDetector));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
